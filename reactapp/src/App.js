8178c75c466450d0c85b3864621d0534766e9aca
Rei <reidumand@outlook.com>
2022-11-05 17:41:51 +0900

    Feat: Add home page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
+// import './App.css';
 import {Routes,Route} from "react-router-dom"
 import { Home } from './pages/Home';
 import { GamePage } from './pages/GamePage';
-
+import './App.css'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='Game Page' element={<GamePage />}/>
-    </Routes>
-    
+    <div className="background">
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='Game Page' element={<GamePage />}/>
+      </Routes>
+    </div>
   );
 }
 
--- reactapp/src/pages/Home.js
+++ reactapp/src/pages/Home.js
@@ -1,55 +1,48 @@
 import { Link } from "react-router-dom"
-import React,{ useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios"
 
+import './Home.css';
 
 
 
-export function Home(){
-  const [game,setGame] = useState("")
-  // const [post, setPost] = React.useState(null);
+export function Home() {
+  const [gameID, setGameID] = useState()
+  const [user, setUser] = useState("");
 
 
-  // React.useEffect(() => {
-  //   axios.get('/newgame').then((response) => {
-  //     setPost(response.data);
-  //   });
-  // }, );
-
- function handleClick() {
-   console.log ("😒")
-   axios.post('https://blind-test-songs-server-predeploy.onrender.com/newgame',{})
-   .then((res) => console.log (res))
-   .catch((err) => console.log (err))
-  }
-  async function handleClick2() {
-    console.log ("😒")
-    await fetch('https://blind-test-songs-server-predeploy.onrender.com/newgame')
-    .then((res) => console.log (res))
-   .catch((err) => console.log (err))
+  useEffect(() => {
+    if (gameID !== undefined) {
+      window.location.replace('/Game Page')
     }
+  }, [gameID])
 
-  
-
- 
-  
-  // axios.get('https://api.github.com/orgs/axios')
-  // .then(response => {
-  //   console.log(response.data);
-  // }, error => {
-  //   console.log(error);
-  // });
-  
-//  if (!post) return null;
-//  console.log (post)
+  async function handleClick() {
+    setGameID(await axios.post('https://blind-test-songs-server-predeploy.onrender.com/newgame', { "username": user })
+      .then((newGameRes) => {
+        const newGameID = newGameRes.data.gameID
+        setGameID(newGameID);
+        return newGameID
+      })
+      .catch((err) => console.log(err))
+    )
+  }
 
   return (
-    <div>
-     
-    <p>Welcome to Blind Test Songs</p>
-      <Link to= "/Game Page">
-        <button onClick={handleClick} >start your game! </button>
-      </Link>
-    </div>
+    <section className="container">
+
+      <h1>BLIND TEST SONGS</h1>
+      <p className="intro-text">You have 30 seconds to guess the name of song, sounds simple right?</p>
+      <p className="intro-text">To start, just write your name and hit the big ol' button.</p>
+      <input className="input"
+        autoFocus
+        type="text"
+        required
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+      />
+
+      <button className="start-button" onClick={handleClick}>Let's go!</button>
+    </section>
   )
 }
