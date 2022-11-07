ba25c282f26cf289db0a64640b9831efc69d43aa
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-11-07 16:54:47 +0900

    feat: change axios endpoint to route endpoint

--- reactapp/src/components/TakeGuess.js
+++ reactapp/src/components/TakeGuess.js
@@ -1,42 +1,49 @@
 import React, { useEffect, useState, useRef } from "react";
-import './TakeGuess.css';
+import "./TakeGuess.css";
 import axios from "axios";
 
-
 export function TakeGuess(props) {
-  const { guess, setGuess, gameID, setSongInfo, setScore, score, setCurrentView, roundSuccess, setRoundSuccess } = props;
+  const {
+    guess,
+    setGuess,
+    gameID,
+    setSongInfo,
+    setScore,
+    score,
+    setCurrentView,
+    roundSuccess,
+    setRoundSuccess,
+  } = props;
 
   const [guessResult, setGuessResult] = useState();
   const inputBox = useRef();
 
   async function sendGuess() {
-    await axios
-      .post('https://blind-test-songs-server-predeploy.onrender.com/guess', { "gameID": gameID, "guess": guess })
-      .then(res => {
-        setGuessResult(res.data.guessMatch)
-        if (res.data.guessMatch === true) {
-          setSongInfo(res.data.result)
-          setScore(res.data.currentScore)
-          setGuess("")
-          setRoundSuccess(res.data.roundSuccess)
-          setCurrentView('round_end')
-        } else {
-          if (inputBox.current) {
-            inputBox.current.classList.add('input-red')
-            setTimeout(() => {
-              inputBox.current.classList.remove('input-red')
-            }, 500);
-          }
+    await axios.post("/guess", { gameID: gameID, guess: guess }).then((res) => {
+      setGuessResult(res.data.guessMatch);
+      if (res.data.guessMatch === true) {
+        setSongInfo(res.data.result);
+        setScore(res.data.currentScore);
+        setGuess("");
+        setRoundSuccess(res.data.roundSuccess);
+        setCurrentView("round_end");
+      } else {
+        if (inputBox.current) {
+          inputBox.current.classList.add("input-red");
+          setTimeout(() => {
+            inputBox.current.classList.remove("input-red");
+          }, 500);
         }
-      })
-    setGuess("")
+      }
+    });
+    setGuess("");
   }
 
   const handleEnter = (event) => {
-    if (event.key === 'Enter' && !roundSuccess) {
-      sendGuess()
+    if (event.key === "Enter" && !roundSuccess) {
+      sendGuess();
     }
-  }
+  };
 
   return (
     <div id="container-input" onKeyDown={handleEnter}>
@@ -51,7 +58,9 @@ export function TakeGuess(props) {
         value={guess}
         onChange={(e) => setGuess(e.target.value)}
       />
-      <button onClick={sendGuess} id="guess-button">GO</button>
+      <button onClick={sendGuess} id="guess-button">
+        GO
+      </button>
     </div>
-  )
+  );
 }
--- reactapp/src/pages/Home.js
+++ reactapp/src/pages/Home.js
@@ -1,8 +1,8 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 
-import './Home.css';
+import "./Home.css";
 
 export function Home() {
   const [user, setUser] = useState("");
@@ -12,40 +12,46 @@ export function Home() {
   const navigate = useNavigate();
   useEffect(() => {
     if (gameID !== undefined) {
-      navigate('/play',{
+      navigate("/play", {
         state: {
           gameID: gameID,
           trackURL: trackURL,
           user: user,
-        }})
+        },
+      });
     }
-  }, [gameID])
+  }, [gameID]);
 
   async function getNewGame() {
-    const startGameData = await axios.post('https://blind-test-songs-server-predeploy.onrender.com/newgame', { "username": user })
+    const startGameData = await axios
+      .post("/newgame", { username: user })
       .then((newGameRes) => newGameRes.data)
-      .catch((err) => console.log(err))
-    setGameID(startGameData.gameID)
-    setTrackURL(startGameData.songURL)
+      .catch((err) => console.log(err));
+    setGameID(startGameData.gameID);
+    setTrackURL(startGameData.songURL);
   }
 
   const handleEnter = (event) => {
-    if (event.key === 'Enter') {
-      getNewGame()
+    if (event.key === "Enter") {
+      getNewGame();
     }
-  }
+  };
 
   return (
     <section className="container" onKeyDown={handleEnter}>
-
       <h1>BLIND TEST SONGS</h1>
-      <p className="intro-text">You have 25 seconds to guess the name of song, sounds simple right?</p>
-      <p className="intro-text">To start, just write your name and hit the big ol' button.</p>
+      <p className="intro-text">
+        You have 25 seconds to guess the name of song, sounds simple right?
+      </p>
+      <p className="intro-text">
+        To start, just write your name and hit the big ol' button.
+      </p>
       {/* <Link 
         to="/play"
         state={{gameID: gameID}}
       ></Link> */}
-      <input className="input"
+      <input
+        className="input"
         onKeyDown={handleEnter}
         autoFocus
         type="text"
@@ -54,7 +60,9 @@ export function Home() {
         onChange={(e) => setUser(e.target.value)}
       />
 
-      <button className="start-button" onClick={getNewGame}>Let's go!</button>
+      <button className="start-button" onClick={getNewGame}>
+        Let's go!
+      </button>
     </section>
-  )
+  );
 }
