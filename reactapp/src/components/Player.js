7728a68e3f782823e752e7fe2c5c2a489178d018
NicoletaBararu <nicoletabararu12@gmail.com>
2022-11-09 11:24:25 +0900

    fix: volume button and change buttons styles in Player.css

--- reactapp/src/components/Player.js
+++ reactapp/src/components/Player.js
@@ -4,18 +4,13 @@ import "./Player.css"
 export function Player(props) {
   const { trackURL } = props;
   const audio = useRef();
-   let volume
-
+  const volumeRef = useRef(50);
+   let volume;
+     
   const [audioMuted, setAudioMute] = useState(false);
-  const [audioVolume, setAudioVolume] = useState(0);
-
-
-   
+ 
   function getSoundAndFadeAudio(audio) {
-
     let fadeUp = setInterval(() => {
-      //console.log(audio);
-      //console.log(audio.current.volume);
       if (audio.current === null) return;
       if (volume !== 0) volume = audio.current.volume * 100 + 2;
       audio.current.volume = Math.round(volume) / 100;
@@ -24,8 +19,6 @@ export function Player(props) {
 
     let fadeDown = setInterval(() => {
       if (audio.current === null) return;
-      // The audio preview is 30sec, change the minus value to match to the timer
-      // TODO: needs a better implementation, check the timer to fade out
       if (audio.current.currentTime > audio.current.duration - 10) {
         volume = audio.current.volume * 100 - 2;
         audio.current.volume = Math.round(volume) / 100;
@@ -41,34 +34,30 @@ export function Player(props) {
   }, [audio]);
 
   let onMute = () => {
-   if (audio.current === null) return;
-   audio.current.muted =  !audioMuted;  
-   setAudioMute(!audioMuted);
-  }
-
-
-  
+    if (audio.current === null) return;
+    audio.current.muted =  !audioMuted;  
+    setAudioMute(!audioMuted);
+  };
+ 
   return (
     <div>
       <audio hidden controls autoPlay ref={audio} >
-        <source src={trackURL} type="audio/mp3"></source>
+         <source src={trackURL} type="audio/mp3"></source>
       </audio>
-      <button onClick={onMute} id="mute-button">🔇</button>             
+      <button onClick={onMute} id="mute-button"> 🔇 </button>             
       <button  id="volume-button"> 🔉
-         <input
-          type="range"
-          min={0}
-          max={10}
-          step={0.7} 
-          value={volume}
-          onChange={(event) => {
-            console.log(" 📣", event)
-          console.log(event.target.valueAsNumber)
-          setAudioVolume(event.target.value);
-  }}
-        />🔊 
+           <input
+            type="range"
+            min={0}
+            max={30}
+            step={0.7} 
+            value={volume}
+            onChange={(event) => {
+            volumeRef.current = event.target.value;
+            audio.current.volume = volumeRef.current;
+          }}
+        /> 🔊 
       </button>
-     
     </div>
   );
 }
