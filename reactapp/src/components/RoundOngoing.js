207264e0e8e2da29b460685c8f85ecf01ce5357c
haruna <haruna.kawakami867@gmail.com>
2022-11-11 13:10:04 +0900

    fix url

--- reactapp/src/components/RoundOngoing.js
+++ reactapp/src/components/RoundOngoing.js
@@ -22,7 +22,7 @@ export function RoundOngoing(props) {
 
   async function sendTimeout() {
     await axios
-      .post(process.env.REACT_APP_API_URL + "timeout", { gameID: gameID })
+      .post(process.env.REACT_APP_API_URL + "/timeout", { gameID: gameID })
       .then((res) => {
         console.log(res);
         setSongInfo(res.data.result);
